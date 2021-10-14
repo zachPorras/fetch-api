f4b457eb1bbbe6b8a31edcacd17a61d5566be573
backfromsolaris <porraszach@gmail.com>
2021-10-14 15:16:57 -0500

    transaction form bug

--- fetch_points_api/site/routes.py
+++ fetch_points_api/site/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from fetch_points_api.forms import SpendPointsForm, TransactionForm
 
 site = Blueprint('site', __name__, template_folder='site_templates')
 
@@ -7,14 +8,16 @@ site = Blueprint('site', __name__, template_folder='site_templates')
 def home():
     return render_template('index.html')
 
-@site.route('/balances')
+@site.route('/balances', methods=['GET'])
 def balances():
     return render_template('balances.html')
 
-@site.route('/transactions')
+@site.route('/transactions', methods=['POST'])
 def add_transactions():
-    return render_template('transactions.html')
+    form = TransactionForm()
+    return render_template('transactions.html', form = form)
 
-@site.route('/spend_points')
+@site.route('/spend_points', methods=['POST', 'PUT', 'GET'])
 def spend_points():
-    return render_template('spend_points.html')
+    form = SpendPointsForm()
+    return render_template('spend_points.html', form = form)
