2bda5aedc9b524136ddab5de2bcff82c7ddf2776
backfromsolaris <porraszach@gmail.com>
2021-10-15 17:01:21 -0500

    preparing to alter schema

--- fetch_points_api/site/routes.py
+++ fetch_points_api/site/routes.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request, jsonify, redirect
+from flask.helpers import url_for
 from fetch_points_api.forms import SpendPointsForm, AddTransactionsForm, CheckBalances
 from fetch_points_api.models import db, Transactions, Partner, partners_schema
+from sqlalchemy.sql import func
 
 site = Blueprint('site', __name__, template_folder='site_templates')
 
@@ -11,7 +13,7 @@ def home():
 
 @site.route('/balances', methods=['GET'])
 def balances():
-    balances = Partner.query.all()
+    balances = db.session.query(Transactions)
     response = jsonify(partners_schema.dump(balances))
     return response
 
@@ -21,10 +23,18 @@ def add_transactions():
     if request.method == 'POST' and form.validate_on_submit:
         points = form.points.data
         partner_name = form.partner_name.data
-        print(points, partner_name)
-        new_transaction = Transactions(points, partner_name)
-        db.session.add(new_transaction)
-        db.session.commit()
+        partner_exists = Partner.query.filter(Partner.partner_name == partner_name).first()
+        print(f'this partner exists: {partner_exists}')
+        if partner_exists:
+            new_transaction = Transactions(points, partner_name)
+            db.session.add(new_transaction)
+            db.session.commit()
+            return redirect(url_for('site.add_transactions', form = form))
+        else:
+            new_partner = Partner(partner_name, points)
+            db.session.add(new_partner)
+            db.session.commit()
+            return redirect(url_for('site.add_transactions', form = form))
 
     return render_template('add_transactions.html', form = form)
 
