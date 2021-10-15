21b22dea1679149fb9a1e7fe477bfcd9a99fee77
backfromsolaris <porraszach@gmail.com>
2021-10-15 11:29:04 -0500

    db/migrate init

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = "gimme dem points"
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('') ---> still needs completion
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- fetch_points_api/__init__.py
+++ fetch_points_api/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from config import Config
 from .site.routes import site
-
+from .models import db
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
+
+db.init_app(app)
+
+migrate = Migrate(app, db)
+
 
 app.register_blueprint(site)
