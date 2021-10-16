1a31634376f50fabc0e70410eacbaf0686b860c5
backfromsolaris <porraszach@gmail.com>
2021-10-15 21:38:25 -0500

    remove env

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = "gimme dem points"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
